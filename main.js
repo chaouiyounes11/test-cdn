2e7c0653d9efdd35272cc65fdde60d59efbcb45f
chaoui <cyounes@FRC0LM01032.local>
2019-09-20 15:20:27 +0200

    animations done

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
 
+// SMOOTH SCROLL function
 
+$('.js-scrollTo').on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+        if ($(window).width() < 768) {
+          crossAnimClose();
+        }
+      });
+    } // End if
+  });
 
   let menuIsOpen = false;
   const crossAnimOpen = () => {
